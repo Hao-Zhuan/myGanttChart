283e36b5a5fb8ab89e5a4f222107daa72bdf769e
JacobZhuan <z@zhuanhao,tech>
2021-04-22 23:55:44 +0800

    bugFOUND

--- myGC/demo.cpp
+++ myGC/demo.cpp
@@ -5,9 +5,9 @@
 class Event
 {
 public:
-  std::string name;
+  std::string name="test";
 
-  tm startT;
+  tm startT{0,0,0,0,0,0,1,1321,0};
   tm endT;
   bool stat;
   static int seq;
@@ -23,7 +23,7 @@ Event::Event()
   int month, day, hour, min, year = 2021;
   std::string name;
   std::cout << "请输入任务名称" << std::endl;
-  std::cin >> year;
+  std::cin >> name;
   std::cout << "请输入任务开始年份" << std::endl;
   std::cin >> year;
   std::cout << "请输入任务开始月份" << std::endl;
@@ -40,24 +40,24 @@ Event::Event()
   this->startT.tm_mday = day;
   this->startT.tm_hour = hour;
   this->startT.tm_min = min;
-  std::cout << asctime(&startT);
-  getchar();
+  std::cout <<"开始时间为"<<asctime(&startT)<<" over";//TBD
 }
 
 class chart:public Event{
   
 
-}
+};
 
 int main()
 {
   time_t timep;
-  time(&timep); /*获取time_t类型的当前时间*/
+  Event A;
+  //time(&timep); /*获取time_t类型的当前时间*/
   /*用gmtime将time_t类型的时间转换为struct tm类型的时间按，／／没有经过时区转换的UTC时间
       然后再用asctime转换为我们常见的格式 Fri Jan 11 17:25:24 2008
     */
    time_t p;
-  tm* now=gmtime(&p);
-  printf("%s", asctime(gmtime(&timep)));
+ // tm* now=gmtime(&p);
+ // printf("%s", asctime(gmtime(&timep)));
   return 0;
 }
